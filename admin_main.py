9d90a2992ee858d359a060e127159573ac5c7fa7
manal14000-sudo <manal14000@gmail.com>
2025-10-29 03:42:30 +0300

    Delete admin_main.py

--- admin_main.py
+++
@@ -1,5 +0,0 @@
-# توافق خلفي مؤقت لتشغيل لوحة المشرف من المسارات الجديدة
-from admin_bot.main_admin_bot import *
-if __name__ == "__main__":
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
