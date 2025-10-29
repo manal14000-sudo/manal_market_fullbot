29f75c54c6ed212aaca89bc9d3f76496752f4e25
manal14000-sudo <manal14000@gmail.com>
2025-10-29 03:44:16 +0300

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-# توافق خلفي مؤقت لتشغيل لوحة المتداول من المسارات الجديدة
-from trader_bot.main_trader_bot import *
-if __name__ == "__main__":
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
