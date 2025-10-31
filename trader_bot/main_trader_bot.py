db1ca93929cbde8f0731bafdcd99ef4d9ba840fc
manal14000-sudo <manal14000@gmail.com>
2025-11-01 00:12:07 +0300

    Update main_trader_bot.py

--- trader_bot/main_trader_bot.py
+++ trader_bot/main_trader_bot.py
@@ -2,6 +2,7 @@
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+
 from config.settings import get_settings
 from trader_bot.keyboards import main_menu_trader
 from trader_bot.messages import MSG_WELCOME
@@ -9,16 +10,19 @@ from trader_bot.utils_trader import is_user_active
 
 logging.basicConfig(level=logging.INFO)
 
-def main():
+def run_trader_bot():
     s = get_settings()
     bot = Bot(token=s.TRADER_BOT_TOKEN, parse_mode="HTML")
-    dp  = Dispatcher(bot)
+    dp = Dispatcher(bot)
 
     @dp.message_handler(commands=["start"])
     async def start(message: types.Message):
-        await message.answer(MSG_WELCOME, reply_markup=main_menu_trader(is_user_active(message.from_user.id)))
+        await message.answer(
+            MSG_WELCOME,
+            reply_markup=main_menu_trader(is_user_active(message.from_user.id))
+        )
 
     executor.start_polling(dp, skip_updates=True)
 
 if __name__ == "__main__":
-    main()
+    run_trader_bot()
